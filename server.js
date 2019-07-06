a5881c6ebf4d2659a0f7fe79839ba77f7c14d79d
myungho <skqoaudgh2@hanmail.net>
2019-07-06 15:05:37 +0900

    프론트 페이지 작성, 서버사이드 소켓 접속 관련 코드 작성

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
 
 app.get('/', (req, res ,next) => {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected!');
+})
+
+http.listen(3000, () => {
     console.log('express server is opened on port 3000');
 });
